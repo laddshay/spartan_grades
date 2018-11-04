41cc8a3f454673df2b46a06f298591cb5722c142
andrewmcdonald27 <andrewmcdonald27@gmail.com>
2018-11-04 16:00:46 -0500

    Further developed ClassInfo activity to format the details of a selected class more cleanly

--- app/src/main/java/com/example/shayladd/spartan_grades/ClassInfo.java
+++ app/src/main/java/com/example/shayladd/spartan_grades/ClassInfo.java
@@ -12,14 +12,58 @@ public class ClassInfo extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_class_info);
     setTitle("Spartan Grades");
+    TextView course_code = (TextView)findViewById(R.id.course_code);
+    TextView course_title = (TextView)findViewById(R.id.course_title);
+    TextView professor = (TextView)findViewById(R.id.professor);
+    TextView semester = (TextView)findViewById(R.id.semester);
 
     Intent intent = getIntent();
     String[] messages = intent.getStringArrayExtra(SearchResults.key);
-    String to_print = "";
-    for (String s : messages){
-      to_print += s;
+
+    course_code.setText("Course Code: "+ messages[1] + " " + messages[2]);
+    course_title.setText("Course Title: " + messages[3]);
+    String professor_str = getProfessor(messages[4]);
+    professor.setText("Professor(s): " + professor_str);
+    String semester_str = getSemester(messages[0]);
+    semester.setText("Semester: " + semester_str);
+  }
+
+  private String getSemester(String str) {
+    String result = "";
+    String prefix = str.substring(0,2);
+
+    if (prefix.equals("FS")) {
+      result += "Fall ";
+    } else if (prefix.equals("SS")) {
+      result += "Spring ";
+    } else if (prefix.equals("US")) {
+      result += "Summer ";
+    } else {
+      result += "Undefined ";
     }
-    TextView output = (TextView)findViewById(R.id.output);
-    output.setText(to_print);
+
+    result += "20" + str.substring(2);
+    return result;
+  }
+
+  private String getProfessor(String str) {
+    String result = "";
+    String last_ch = " ";
+
+    for (int i = 0; i < str.length(); i++){
+      String ch = str.substring(i, i+1);
+
+      if (ch.equals("|")) {
+        result += "&";
+      } else if (last_ch.equals(" ")) {
+        result += ch.toUpperCase();
+      } else {
+        result += ch.toLowerCase();
+      }
+
+      last_ch = ch;
+    }
+
+    return result;
   }
 }
